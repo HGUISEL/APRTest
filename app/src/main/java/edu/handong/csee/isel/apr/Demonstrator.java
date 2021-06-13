9462f41b8e0c997a63e802f3a4f85035b3d15656
JC <jc@lifove.net>
2021-06-13 09:01:56 +0900

    Buggy change

--- app/src/main/java/edu/handong/csee/isel/apr/Demonstrator.java
+++ app/src/main/java/edu/handong/csee/isel/apr/Demonstrator.java
@@ -5,7 +5,7 @@ package edu.handong.csee.isel.apr;
 
 public class Demonstrator {
     public int add(int one, int two) {
-        return one - two;
+        return one * two;
     }
 
     public static void main(String[] args) {
